65fa9a7e3b8b70341858edec310e219a029f1fc3
dmikhr <dmitrypkh@gmail.com>
2019-07-02 11:04:52 +0300

    rubocop fixes and tests for db columns

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { should have_db_column(:body).of_type(:text) }
+
   it { should belong_to :question }
 
   it { should validate_presence_of :body }
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should have_db_column(:title).of_type(:string) }
+  it { should have_db_column(:body).of_type(:text) }
+
   it { should have_many(:answers).dependent(:destroy) }
 
   it { should validate_presence_of :title }
