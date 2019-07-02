818a318331a08de21111fc2d36d26e83494af2fc
dmikhr <dmitrypkh@gmail.com>
2019-07-02 11:21:41 +0300

    add limitations to Question title and body length

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Question, type: :model do
 
   it { should have_many(:answers).dependent(:destroy) }
 
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+
+  it { should validate_length_of(:title).is_at_least(10).is_at_most(50) }
+  it { should validate_length_of(:body).is_at_least(10).is_at_most(1000) }
 end
