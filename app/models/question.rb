49f42ec6acda2019ea5f20e4d6e5a9796d7aa525
dmikhr <dmitrypkh@gmail.com>
2019-07-02 10:29:39 +0300

    Answer model and its tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
+  # в случае удаления вопроса, связанные ответы также удалятся dependent: :destroy
+  has_many :answers, dependent: :destroy
+
   validates :title, :body, presence: true
 end
