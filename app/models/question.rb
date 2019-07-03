0c9583466e4b5c4d9d89bdef008747e087d73985
dmikhr <dmitrypkh@gmail.com>
2019-07-03 08:11:48 +0300

    fixing issues from previous lesson 8a9fbe4

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,7 @@
 class Question < ApplicationRecord
-  # в случае удаления вопроса, связанные ответы также удалятся dependent: :destroy
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
-  # сделаем ограничение на размер заголовка вопроса
-  # чтобы пользователи давали вопросам осмысленные названия, но при этом не слишком длинные
-  validates :title, length: { in: 10..50 }
-  # ограничим размеры самого вопроса
-  validates :body, length: { in: 10..1000 }
+  validates :title, length: { in: 10..150 }
+  validates :body, length: { minimum: 10 }
 end
