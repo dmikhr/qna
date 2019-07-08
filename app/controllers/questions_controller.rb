c4674d28625c8658b0186e4cee9adb4447e1899a
dmikhr <dmitrypkh@gmail.com>
2019-07-08 11:55:08 +0300

    Story authenticated user asks a question from screencast

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,19 @@
 class QuestionsController < ApplicationController
 
+  before_action :load_question, only: %i[show]
+
   def index
     @questions = Question.all
   end
 
-  def new; end
+  def new
+    @question = Question.new
+  end
 
   def create
     @question = Question.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
@@ -21,5 +25,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body)
+  end
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 end
