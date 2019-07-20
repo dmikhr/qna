e7b1de82fb9fb2460076093070e19fbe25c732db
dmikhr <dmitrypkh@gmail.com>
2019-07-20 20:07:38 +0300

    limit files controller parameters to id

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,7 +1,13 @@
 class FilesController < ApplicationController
 
   def destroy
-    @file = ActiveStorage::Attachment.find(params[:id])
+    @file = ActiveStorage::Attachment.find(file_id)
     @file.purge if current_user&.author_of?(@file.record)
+  end
+
+  private
+
+  def file_id
+    params.require(:id)
   end
 end
