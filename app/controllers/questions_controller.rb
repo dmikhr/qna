80bc47383570f70fc416bed08b6357e21eb7dee8
dmikhr <dmitrypkh@gmail.com>
2019-07-23 16:33:40 +0300

    Add link in a nested form screencast

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.links.new
   end
 
   def create
@@ -36,7 +37,8 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title, :body,
+                                      files: [], links_attributes: [:name, :url])
   end
 
   def load_question
