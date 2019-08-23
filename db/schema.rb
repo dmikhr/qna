26a07e1d887de824ba51042335aa183c1029d7da
dmikhr <dmitrypkh@gmail.com>
2019-08-23 15:12:05 +0300

    add null constraints to authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_081718) do
+ActiveRecord::Schema.define(version: 2019_08_23_120526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2019_08_22_081718) do
 
   create_table "authorizations", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "provider"
-    t.string "uid"
+    t.string "provider", null: false
+    t.string "uid", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid"
