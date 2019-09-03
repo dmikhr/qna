fb681533c3ec87400d4cb2a2820cc1f26754dfb1
dmikhr <dmitrypkh@gmail.com>
2019-09-03 08:49:03 +0300

    api implementation from screencast

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,16 @@ Rails.application.routes.draw do
   resources :links, only: :destroy
   resources :rewards, only: :index
 
+  namespace :api do
+    namespace :v1 do
+      resources :profiles, only: [] do
+        get :me, on: :collection
+      end
+
+      resources :questions, only: [:index]
+    end
+  end
+
   mount ActionCable.server => '/cable'
+
 end
