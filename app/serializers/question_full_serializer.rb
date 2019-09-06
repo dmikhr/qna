3cd6956aee8e56ae3a5361d15a222a04b27983d3
dmikhr <dmitrypkh@gmail.com>
2019-09-06 12:39:19 +0300

    move common parts of serializers to concern

--- app/serializers/question_full_serializer.rb
+++ app/serializers/question_full_serializer.rb
@@ -1,21 +1,5 @@
 class QuestionFullSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
+  include Serialized
 
   attributes :id, :title, :body, :created_at, :updated_at
-  has_many :links
-  has_many :comments
-  has_many :files
-
-  def comments
-    object.comments.map { |comment| comment.body }
-  end
-
-  def links
-    object.links.map { |link| link.url }
-  end
-
-  def files
-    # https://github.com/rails/rails/issues/31581#issuecomment-354397873
-    object.files.map { |file| rails_blob_path(file, only_path: true) }
-  end
 end
