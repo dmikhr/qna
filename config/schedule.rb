8eb85f9bc8a28c0320d548125e18b977b385d125
dmikhr <dmitrypkh@gmail.com>
2019-09-15 13:49:23 +0300

    call service directly in schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,8 @@
 #   rake "some:great:rake:task"
 # end
 #
-every 1.day do
-  runner "DailyDigestJob.perform_now"
+every 1.day, at: '8pm' do
+  runner "Services::DailyDigest.call"
 end
 
 # Learn more: http://github.com/javan/whenever
