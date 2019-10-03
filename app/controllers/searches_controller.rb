bfdfc5033822fa35b7f47974ec1f0e0a3325905a
dmikhr <dmitrypkh@gmail.com>
2019-10-03 12:16:56 +0300

    minor fixes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,10 +3,7 @@ class SearchesController < ApplicationController
   skip_authorization_check
 
   def index
-    # byebug
     if params[:search].present?
-      # @search_results = Question.search(params[:search])
-      # byebug
       @search_results = Services::Search.call(search_categories, params[:search])
     else
       render :index
@@ -14,13 +11,6 @@ class SearchesController < ApplicationController
   end
 
   private
-
-  # <ActionController::Parameters {"utf8"=>"✓", "search"=>"111", "question"=>"true", "commit"=>"Find", "controller"=>"searches", "action"=>"index"} permitted: false>
-
-  # Question 111
-  # Answer qwertyuioi
-  # Comment 1qwee
-  # User test
 
   def search_categories
     input_params = []
