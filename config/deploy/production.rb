c0482a066c788efc3077cbe0e8b86559b261b8b3
dmikhr <dmitrypkh@gmail.com>
2019-10-07 16:56:12 +0300

    sidekiq config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,8 @@
 server "104.248.83.204", user: "deployer", roles: %w{app db web}, primary: true
 set :rail_env, :production
 
+set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
+
 
 # Custom SSH Options
 # ==================
